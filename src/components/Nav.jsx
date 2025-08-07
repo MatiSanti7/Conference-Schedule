a3f4c5b71752f74e01f9daea739825d30b266efa
MatiSanti7 <matiassantillan052@gmail.com>
2025-08-07 17:59:09 -0300

    Actualizar el componente de navegación: agregar enlaces de Contacto y Registro, y mejorar la estructura de la lista.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,15 +11,20 @@ const Nav = () => {
         <a href="/">
           <img src={isologo} alt="" className="h-20" />
         </a>
-        <ul className="flex gap-16 text-white">
-          <li className="text-xl transition-all duration-300 hover:text-red-600">
-            <a href="/home">Home</a>
-          </li>
+        <ul className="flex items-center h-12 gap-16 text-white">
           <li className="text-xl transition-all duration-300 hover:text-red-600">
             <a href="/about">About</a>
           </li>
           <li className="text-xl transition-all duration-300 hover:text-red-600">
             <a href="/schedule">Schedule</a>
+          </li>
+          <li className="text-xl transition-all duration-300 hover:text-red-600">
+            <a href="/schedule">Contact</a>
+          </li>
+          <li className="flex items-center h-full text-xl font-bold text-black transition-all duration-300 bg-white rounded-full cursor-pointer hover:bg-red-600 hover:text-white hover:shadow-slate-400/50 hover:shadow-md">
+            <a href="/schedule" className="px-10">
+              Register
+            </a>
           </li>
         </ul>
       </div>
