4b93352dda3f68c174d489b7d27ccc3296deae72
MatiSanti7 <matiassantillan052@gmail.com>
2025-08-17 17:26:23 -0300

    Agregar animaciones AOS y ajustar la visualización de patrocinadores en el componente SectionSponsors

--- src/components/SectionSponsors.jsx
+++ src/components/SectionSponsors.jsx
@@ -1,7 +1,11 @@
 import SponsorsData from "../assets/data/sponsors.json";
+import AOS from "aos";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
+import "aos/dist/aos.css";
+
+AOS.init();
 
 export default function SectionSponsors() {
   const boxes = SponsorsData.result.list.map((sponsor) => sponsor);
@@ -14,7 +18,7 @@ export default function SectionSponsors() {
         className="h-[17rem] w-full"
         modules={[Autoplay]}
         loop={true}
-        slidesPerView={4} // cuántas cajas se ven al mismo tiempo
+        slidesPerView={6} // cuántas cajas se ven al mismo tiempo
         spaceBetween={20}
         autoplay={{
           delay: 2000, // cada 2s avanza
@@ -24,10 +28,12 @@ export default function SectionSponsors() {
       >
         {boxes.map((num) => (
           <SwiperSlide key={num}>
-            <div className="flex flex-col items-center w-56 h-64 gap-6 px-3 py-5 text-xl text-white transition-all duration-300 border-2 rounded-md bg-black/70 backdrop-blur-sm hover:cursor-default hover:shadow-md border-white/20 hover:border-white/10 hover:shadow-white">
-              <img src={num.image} alt="" className="object-contain h-3/5 " />
-              <p className="text-2xl text-center">{num.name}</p>
-            </div>
+            <a href={num.url} target="_blank" data-aos="fade-up">
+              <div className="flex flex-col items-center w-56 h-64 gap-6 px-3 py-5 text-xl text-white transition-all duration-300 border-2 rounded-md bg-black/70 backdrop-blur-sm hover:shadow-md border-white/20 hover:border-white/10 hover:shadow-white">
+                <img src={num.image} alt="" className="object-contain h-3/5 " />
+                <p className="text-2xl text-center">{num.name}</p>
+              </div>
+            </a>
           </SwiperSlide>
         ))}
       </Swiper>
