d01a2601aeaa9a17efebb88387403b1f4a0e4f30
MatiSanti7 <matiassantillan052@gmail.com>
2025-08-25 21:34:51 -0300

    Agregar gestión de estado y navegación dinámica en el componente CMS para mejorar la interacción del usuario.

--- src/pages/CMS.jsx
+++ src/pages/CMS.jsx
@@ -1,11 +1,13 @@
 import NavCMS from "../components/NavCMS";
 import MainCMS from "../components/MainCMS";
+import { useState } from "react";
 
 export default function CMS() {
+  const [selected, setSelected] = useState("eventInfo");
   return (
-    <div className="flex h-screen bg-slate-900">
-      <NavCMS />
-      <MainCMS />
+    <div className="flex h-full bg-slate-900">
+      <NavCMS onSelect={setSelected} />
+      <MainCMS selected={selected} />
     </div>
   );
 }
