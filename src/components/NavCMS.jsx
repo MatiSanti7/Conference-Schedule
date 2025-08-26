695f618cf32286f37132e19c69f121934d533dd0
MatiSanti7 <matiassantillan052@gmail.com>
2025-08-25 21:34:11 -0300

    Actualizar el componente NavCMS para incluir navegación dinámica y mejorar la interacción del usuario, añadiendo propTypes y gestionando el estado de los elementos seleccionados.

--- src/components/NavCMS.jsx
+++ src/components/NavCMS.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
-export default function NavCMS() {
+export default function NavCMS({ onSelect }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [hoveredItem, setHoveredItem] = useState(null);
 
   return (
     <nav
-      className={`absolute flex flex-col justify-between h-screen py-20 px-5 text-2xl text-white transition-all duration-100 bg-gray-700 ${
+      className={`fixed flex flex-col justify-between h-screen py-20 px-5 text-2xl text-white transition-all duration-100  bg-gray-700 ${
         isHovered ? "w-auto" : "w-44"
       }`}
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      onMouseLeave={() => {
+        setIsHovered(false);
+        setHoveredItem(null);
+      }}
     >
-      <ul className="flex flex-col gap-3">
-        <li className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600">
+      <ul className="flex flex-col gap-4">
+        <li
+          className="relative flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600"
+          onClick={() => {
+            setHoveredItem(hoveredItem === "home" ? null : "home");
+          }}
+        >
           <i className="fa fa-home" aria-hidden="true"></i>
           <span
             className={`transition-all duration-300 pl-16 ${
@@ -23,8 +33,49 @@ export default function NavCMS() {
           >
             Home
           </span>
+          {hoveredItem === "home" && (
+            <div className="absolute top-0 ml-2 rounded-lg shadow-lg left-full bg-slate-700">
+              <ul className="flex flex-col gap-2 p-2 w-60">
+                <li
+                  onClick={() => {
+                    onSelect("eventInfo");
+                    setHoveredItem(null);
+                    setIsHovered(false);
+                  }}
+                  className="py-2 pl-4 rounded-lg cursor-pointer hover:bg-slate-600"
+                >
+                  Event Information
+                </li>
+                <li
+                  onClick={() => {
+                    onSelect("speakersInfo");
+                    setHoveredItem(null);
+                    setIsHovered(false);
+                  }}
+                  className="py-2 pl-4 rounded-lg cursor-pointer hover:bg-slate-600"
+                >
+                  Speakers Information
+                </li>
+                <li
+                  onClick={() => {
+                    onSelect("sponsorsInfo");
+                    setHoveredItem(null);
+                    setIsHovered(false);
+                  }}
+                  className="py-2 pl-4 rounded-lg cursor-pointer hover:bg-slate-600"
+                >
+                  Sponsors Information
+                </li>
+              </ul>
+            </div>
+          )}
         </li>
-        <li className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600">
+        <li
+          className="relative flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600"
+          onClick={() => {
+            setHoveredItem(hoveredItem === "about" ? null : "about");
+          }}
+        >
           <i className="fa fa-info-circle" aria-hidden="true"></i>
           <span
             className={`transition-all duration-300 pl-16 ${
@@ -35,8 +86,39 @@ export default function NavCMS() {
           >
             About
           </span>
+          {hoveredItem === "about" && (
+            <div className="absolute top-0 ml-2 rounded-lg shadow-lg left-full bg-slate-700">
+              <ul className="flex flex-col gap-2 p-2 w-60">
+                <li
+                  onClick={() => {
+                    onSelect("mainAbout");
+                    setHoveredItem(null);
+                    setIsHovered(false);
+                  }}
+                  className="py-2 pl-4 rounded-lg cursor-pointer hover:bg-slate-600"
+                >
+                  About Information
+                </li>
+                <li
+                  onClick={() => {
+                    onSelect("faqAbout");
+                    setHoveredItem(null);
+                    setIsHovered(false);
+                  }}
+                  className="py-2 pl-4 rounded-lg cursor-pointer hover:bg-slate-600"
+                >
+                  FAQ Information
+                </li>
+              </ul>
+            </div>
+          )}
         </li>
-        <li className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600">
+        <li
+          className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600"
+          onClick={() => {
+            onSelect("schedule");
+          }}
+        >
           <i className="fa fa-calendar-minus-o" aria-hidden="true"></i>
           <span
             className={`transition-all duration-300 pl-16 ${
@@ -48,10 +130,15 @@ export default function NavCMS() {
             Schedule
           </span>
         </li>
-        <li className="w-[400px] flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600">
+        <li
+          className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600"
+          onClick={() => {
+            onSelect("AM");
+          }}
+        >
           <i className="fas fa-hotel "></i>
           <span
-            className={`w-[310px] transition-all duration-300 pl-16 ${
+            className={` transition-all duration-300 pl-16 whitespace-nowrap ${
               isHovered
                 ? "opacity-100 visible translate-x-0"
                 : "opacity-0 invisible -translate-x-20"
@@ -60,28 +147,33 @@ export default function NavCMS() {
             Accommodation Meals
           </span>
         </li>
-        <li className="w-[400px] flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600">
+        <li
+          className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600 "
+          onClick={() => {
+            onSelect("CodeConduct");
+          }}
+        >
           <i className="fa fa-book" aria-hidden="true"></i>
           <span
-            className={`transition-all duration-300 pl-16 ${
+            className={`transition-all duration-300 pl-16 whitespace-nowrap ${
               isHovered
                 ? "opacity-100 visible translate-x-0"
                 : "opacity-0 invisible -translate-x-20"
             }`}
           >
-            Code of Coduct
+            Code of Conduct
           </span>
         </li>
       </ul>
       <button
-        className="w-[400px] flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600"
+        className="flex items-center p-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-slate-600"
         onClick={() => {
           window.location.href = "/";
         }}
       >
         <i className="fa fa-sign-out" aria-hidden="true"></i>
         <span
-          className={`transition-all duration-300 pl-16 ${
+          className={`transition-all duration-300 pl-16 whitespace-nowrap ${
             isHovered
               ? "opacity-100 visible translate-x-0"
               : "opacity-0 invisible -translate-x-20"
@@ -93,3 +185,7 @@ export default function NavCMS() {
     </nav>
   );
 }
+
+NavCMS.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+};
