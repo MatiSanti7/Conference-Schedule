500f78057ada909b397d2cbbaa597b230099a416
MatiSanti7 <matiassantillan052@gmail.com>
2025-08-25 21:34:37 -0300

    Agregar el componente MainCMS con navegación dinámica y validación de props para mejorar la estructura y la experiencia del usuario.

--- src/components/MainCMS.jsx
+++ src/components/MainCMS.jsx
@@ -1,3 +1,27 @@
-export default function MainCMS() {
-  return <div className="w-full text-white ml-28"></div>;
+import EventCMS from "./EventCMS";
+import SpeakersCMS from "./SpeakersCMS";
+import SponsorsCMS from "./SponsorsCMS";
+import MainAboutCMS from "./MainAboutCMS";
+import FaqAboutCMS from "./FaqAboutCMS";
+import PropTypes from "prop-types";
+import ScheduleCMS from "./ScheduleCMS";
+import AMCMS from "./AMCMS";
+import CodeConductCMS from "./CodeConductCMS";
+export default function MainCMS({ selected }) {
+  const pages = {
+    eventInfo: <EventCMS />,
+    speakersInfo: <SpeakersCMS />,
+    sponsorsInfo: <SponsorsCMS />,
+    mainAbout: <MainAboutCMS />,
+    faqAbout: <FaqAboutCMS />,
+    schedule: <ScheduleCMS />,
+    AM: <AMCMS />,
+    CodeConduct: <CodeConductCMS />,
+  };
+
+  return <main className="w-full h-full p-24 ml-44">{pages[selected]}</main>;
 }
+
+MainCMS.propTypes = {
+  selected: PropTypes.string.isRequired,
+};
